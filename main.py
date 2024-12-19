29ed49e2e0ba1262d8b3aa6d5b32616cfba0158a
Akshay Kumar <akshaykr7775@gmail.com>
2024-12-20 03:01:53 +0530

    first train

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
-import tensorflow as tf
-import numpy
-import xml.etree.ElementTree as ET
 
-classes=['aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair','cow','diningtable',
-         'dog','horse','motorbike','person','pottedplant','sheep','sofa','train','tvmonitor']
+from ultralytics import YOLO
 
-print(f"Total no. of classes")
+model = YOLO("yolov8n.yaml")  # build a new model - nano version
 
-def preprocess_xml(filename):
-  tree = ET.parse(filename)
-  root = tree.getroot()
-  size_tree = root.find('size')
-  height = float(size_tree.find('height').text)  # height of image
-  width = float(size_tree.find('width').text)   # .text retrieves the string between <height> and </height>.
-  bounding_boxes=[]
-  for object_tree in root.findall('object'):
-    for bounding_box in object_tree.iter('bndbox'):
-      xmin = (float(bounding_box.find('xmin').text))
-      ymin = (float(bounding_box.find('ymin').text))
-      xmax = (float(bounding_box.find('xmax').text))
-      ymax = (float(bounding_box.find('ymax').text))
-      break                                         # break -  to take only 1st bb for one object, in case of more than 1 bb
-    class_name = object_tree.find('name').text
-    class_dict = {classes[i]:i for i in range(len(classes))}  # {'object_name': 0, ...}
-    bounding_box = [
-        (xmin+xmax)/(2*width), (ymin+ymax)/(2*height), (xmax-xmin)/width,   # normalizing BB values with width, height of image according to yolo algo.
-        (ymax-ymin)/height, class_dict[class_name]]
-    bounding_boxes.append(bounding_box)
-  return tf.convert_to_tensor(bounding_boxes)
-
-
-bb = preprocess_xml("D:/Lab Work/ML_Projects/ML_Data/Object Detection Datasets/VOC2012/Annotations/2007_000027.xml")
-print(bb)
-
+results = model.train(data='config.yaml', epochs=5)
